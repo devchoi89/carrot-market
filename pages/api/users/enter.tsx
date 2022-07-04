d62476be3301eff3f4549255c66171b8635b02c7
devchoi89 <devchoi89@gmail.com>
2022-07-05 00:26:29 +0900

    nodemailer setting

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,6 +1,10 @@
+import twilio from "twilio";
 import client from "@libs/server/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+import smtpTransport from "@libs/server/mail";
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   req: NextApiRequest,
@@ -26,7 +30,34 @@ async function handler(
       },
     },
   });
-  console.log(token);
+  if (phone) {
+    await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSG_SID,
+      to: process.env.PHONE!, //env 값이 없을 수도 있어 오류가 남. 느낌표를 붙여 존재하는 값임을 알려준다.
+      body: `로그인 토큰: ${payload}.`,
+    });
+  } else if (email) {
+    const mailOptions = {
+      from: "blacktea37@naver.com",
+      to: "devchoi89@gmail.com",
+      subject: "Nomad Carrot Authentication Email",
+      text: `Authentication Code : ${payload}`,
+    };
+    const result = await smtpTransport.sendMail(
+      mailOptions,
+      (error, responses) => {
+        if (error) {
+          console.log(error);
+          return null;
+        } else {
+          console.log(responses);
+          return null;
+        }
+      }
+    );
+    smtpTransport.close();
+    console.log(result);
+  }
   /*  if (email) {
     user = await client.user.findUnique({
       where: {
